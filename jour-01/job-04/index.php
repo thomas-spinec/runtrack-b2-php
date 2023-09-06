602db5b2129426928abbd8254c441677bf076703
thomas-spinec <thomas.spinec@laplateforme.io>
2023-09-06 13:40:39 +0200

    correction du job04 pour enlever la fonction système

--- jour-01/job-04/index.php
+++ jour-01/job-04/index.php
@@ -1,17 +1,18 @@
 <?php
+// sans utiliser la fonction array_push
     function my_fizz_buzz(int $length): array{
         $result = [];
         for ($i = 1; $i <= $length; $i++){
             if ($i % 3 == 0 && $i % 5 == 0){
-                array_push($result, "FizzBuzz");
+                $result[] = "FizzBuzz";
             } elseif ($i % 3 == 0){
-                array_push($result, "Fizz");
+                $result[] = "Fizz";
             } elseif ($i % 5 == 0){
-                array_push($result, "Buzz");
+                $result[]= "Buzz";
             } else {
-                array_push($result, $i);
+                $result[]= $i;
             }
         }
         return $result;
     }
-//    var_dump(my_fizz_buzz(100));
+    var_dump(my_fizz_buzz(100));
