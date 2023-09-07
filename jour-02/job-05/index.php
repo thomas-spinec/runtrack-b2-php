f3d14c078783944da7cb7ca76b7dcdc5ea5d4d4b
thomas-spinec <thomas.spinec@laplateforme.io>
2023-09-07 20:11:43 +0200

    jour02 job05 done

--- jour-02/job-05/index.php
+++ jour-02/job-05/index.php
@@ -14,6 +14,67 @@ function find_full_rooms() {
         echo "Error: " . $e->getMessage();
     }
 
-    // requête SQL
-    $sql = "SELECT room.name, COUNT(student.id) as nb_students FROM `room` INNER JOIN `student` ON room.id = student.room_id GROUP BY room.name HAVING nb_students >= 10";
+    // On récupère le nom des salles, leur capacité, puis on récupère la promotion liée à cette salle, afin de compter le nombre d'étudiants dans cette promotion
+    $sql = "SELECT room.name, room.capacity, IF(COUNT(student.id) >= room.capacity, 'Oui', 'Non') as is_full FROM `room` INNER JOIN `grade` ON grade.room_id = room.id INNER JOIN `student` ON grade.id = student.grade_id GROUP BY room.id";
+    $select = $conn->prepare($sql);
+    $select->execute();
+    return $select->fetchAll(PDO::FETCH_ASSOC);
 }
+
+$rooms = find_full_rooms();
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+
+            table {
+                border-collapse: collapse;
+            }
+
+            th, td {
+                border: 1px solid black;
+                padding: 5px;
+            }
+
+            thead th {
+                background-color: #00BFFF;
+            }
+
+    </style>
+    <title>Jour02 job05</title>
+</head>
+<body>
+<header>
+    <H1>Jour02 -- job05</H1>
+</header>
+
+<main>
+    <table>
+        <thead>
+        <tr>
+            <th>Nom</th>
+            <th>Capacité</th>
+            <th>Complet</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($rooms as $room): ?>
+            <tr>
+                <td><?= $room['name'] ?></td>
+                <td><?= $room['capacity'] ?></td>
+                <td><?= $room['is_full'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</main>
+
+</body>
+</html>
